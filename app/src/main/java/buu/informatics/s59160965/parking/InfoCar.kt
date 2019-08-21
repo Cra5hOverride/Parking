560ce16976a2e16a8c5558760aaa51581796ad40
Phitchaya Krobphet <phichaya6310@gmail.com>
2019-08-21 18:47:15 +0700

    add button

---
+++ app/src/main/java/buu/informatics/s59160965/parking/InfoCar.kt
@@ -0,0 +1,3 @@
+package buu.informatics.s59160965.parking
+
+data class InfoCar(var licenseplate: String = "", var name: String = "", var brand: String = "")
--- app/src/main/java/buu/informatics/s59160965/parking/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160965/parking/MainActivity.kt
@@ -2,10 +2,14 @@ package buu.informatics.s59160965.parking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import buu.informatics.s59160965.parking.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
